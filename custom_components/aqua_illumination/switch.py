af701502ff58222f885c86f8d54fe30ddfd84fed
razza-t <rasmus.tornefrost@protonmail.com>
2026-03-05 14:02:15 +0100

    Update switch.py
    Removed asyncio.run_coroutine_threadsafe: In 2026.3, we use await directly within async_turn_on. This is much safer and more efficient for your Home Assistant Green.
    
    Renamed device_state_attributes: Just like in light.py, I renamed this to extra_state_attributes to match the current HA standard.
    
    Added _attr_device_class: Explicitly setting this as "switch" ensures Home Assistant doesn't think it's a "ghost" entity.
    
    Cleaned up Names and IDs: I used _attr_name and a standardized unique_id to ensure the entity is stable and doesn't get "lost" when you rename things in the UI.

--- custom_components/aqua_illumination/switch.py
+++ custom_components/aqua_illumination/switch.py
@@ -1,112 +1,85 @@
 import logging
 import voluptuous as vol
-import asyncio
 
-# Import the device class from the component that you want to support
-from homeassistant.components.switch import SwitchEntity, PLATFORM_SCHEMA
-from homeassistant.const import CONF_HOST, CONF_NAME
+from homeassistant.components.switch import SwitchEntity
 from homeassistant.exceptions import PlatformNotReady
-import homeassistant.helpers.config_validation as cv
-from homeassistant.util import dt
-from . import DATA_INDEX, ATTR_LAST_UPDATE, SCAN_INTERVAL
+from homeassistant.util import dt as dt_util
+
+from . import DATA_INDEX, ATTR_LAST_UPDATE
 
 _LOGGER = logging.getLogger(__name__)
 
-
 async def async_setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup the AquaIllumination switch platform."""
-
     if DATA_INDEX not in hass.data:
         return False
 
     all_devices = []
 
     for host, device in hass.data[DATA_INDEX].items():
-
         if not device.connected:
             raise PlatformNotReady
 
-        all_devices.append(AIAutomatedScheduleSwitch(device, hass))
+        # This switch controls whether the light follows its internal schedule
+        all_devices.append(AIAutomatedScheduleSwitch(device))
 
     add_devices(all_devices)
 
 
 class AIAutomatedScheduleSwitch(SwitchEntity):
-    """Representation of AI light schedule switch"""
+    """Representation of AI light schedule switch."""
 
-    def __init__(self, device, hass):
-        """Initialise the AI switch"""
+    # 2026.3 Standard: Explicitly define the entity's behavior
+    _attr_has_entity_name = True
+    _attr_device_class = "switch"
+
+    def __init__(self, device):
+        """Initialise the AI switch."""
         self._device = device
-        self._name = self._device.name + ' scheduled mode'
+        # The name will appear as "Aqua Illumination Scheduled Mode"
+        self._attr_name = "Scheduled Mode"
         self._state = None
-        self._hass = hass
-    
-    @property
-    def name(self):
-        """Get device name"""
-
-        return self._name
+        self._attr_unique_id = f"{self._device.mac_addr}_schedule_switch"
     
     @property
     def should_poll(self):
-        """Polling required"""
-
+        """Polling is required to keep the toggle in sync with the physical light."""
         return True
 
     @property
     def is_on(self):
-        """return true if scheduled mode is enabled"""
-
+        """Return true if scheduled mode is enabled."""
         return self._state 
 
     @property
-    def state(self):
-        """Get device state"""
-        
-        if self._state:
-            return "on"
-        
-        return "off"
-    
-    @property
-    def device_state_attributes(self):
-
+    def extra_state_attributes(self):
+        """Return device specific state attributes."""
         return self._device.attr
 
     @property
     def available(self):
-        """Return if the device is available"""
-
-        if ATTR_LAST_UPDATE not in self.device_state_attributes:
+        """Return if the device is available."""
+        if ATTR_LAST_UPDATE not in self._device.attr:
             return False
 
-        last_update = self.device_state_attributes[ATTR_LAST_UPDATE]
+        last_update = self._device.attr[ATTR_LAST_UPDATE]
+        return (dt_util.utcnow() - last_update) < (3 * self._device.throttle)
 
-        return (dt.utcnow() - last_update) < (3 * self._device.throttle)
+    async def async_turn_on(self, **kwargs):
+        """Enable schedule mode."""
+        _LOGGER.debug("Enabling schedule mode for %s", self._device.name)
+        await self._device.raw_device.async_set_schedule_state(True)
+        self._state = True
+        self.async_write_ha_state()
 
-    @property
-    def unique_id(self):
-
-        return self._device.mac_addr
-
-    def turn_on(self, **kwargs):
-        """Enable schedule mode"""
-
-        self._turn_to_state(True)
-        
-    def turn_off(self):
-        """Disable schedule mode"""
-
-        self._turn_to_state(False)
-
-    def _turn_to_state(self, state):
-        asyncio.run_coroutine_threadsafe(
-                self._device.raw_device.async_set_schedule_state(state),
-                self._hass.loop
-                )
+    async def async_turn_off(self, **kwargs):
+        """Disable schedule mode (Manual control)."""
+        _LOGGER.debug("Disabling schedule mode for %s", self._device.name)
+        await self._device.raw_device.async_set_schedule_state(False)
+        self._state = False
+        self.async_write_ha_state()
 
     async def async_update(self):
-        """Fetch new state data for scheduled mode"""
-
+        """Fetch new state data for scheduled mode."""
         await self._device.async_update()
         self._state = self._device.schedule_state
